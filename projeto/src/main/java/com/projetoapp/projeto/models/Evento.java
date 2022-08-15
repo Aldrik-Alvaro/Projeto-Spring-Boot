39fa4195e3d1ba4e95390c18936b180f24699ec3
AldrikAlvaro <51426536+Aldrik-Alvaro@users.noreply.github.com>
2022-08-15 12:22:17 -0300

    aula 3

--- projeto/src/main/java/com/projetoapp/projeto/models/Evento.java
+++ projeto/src/main/java/com/projetoapp/projeto/models/Evento.java
@@ -1,15 +1,39 @@
 package com.projetoapp.projeto.models;
 
-public class Evento {
+//import java.io.Serializable;
+
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+
+//@Entity
+public class Evento{
+	
+	//GERAÇÃO DE UM CAMPO AUTO INCREMENT HÁ TABELA
+	//implements Serializable
+	/*private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long codigo;
+	
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	*/
+	////////////////////////////////////////////////
 	
 	//DEFINE AS COLUNAS DA TABELA
 	private String nome;
+	
+
 	private String local;
 	private String data;
 	private String horario;
-	
-	
-	
 	
 	public String getNome() {
 		return nome;
