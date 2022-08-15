50b76e9cf90fccf8e0fc61e5f982dc344338f1f7
AldrikAlvaro <51426536+Aldrik-Alvaro@users.noreply.github.com>
2022-08-15 09:43:05 -0300

    aula 2

--- projeto/src/main/java/com/projetoapp/projeto/controllers/IndexController.java
+++ projeto/src/main/java/com/projetoapp/projeto/controllers/IndexController.java
@@ -1,14 +1,15 @@
-package com.projetoapp.projeto;
+package com.projetoapp.projeto.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class indexController {
+public class IndexController {
 	
 	//REDIRECIONA PARA PAGINA INDEX HTML
 	@RequestMapping("/")
 	public String index() {
 		return "index";
 	}
+
 }
