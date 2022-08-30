71e60c2cb07c2f4d36b548d7d8c15f1348a3fae3
AldrikAlvaro <51426536+Aldrik-Alvaro@users.noreply.github.com>
2022-08-30 16:33:13 -0300

    ajustes

--- projeto/src/main/java/com/fluffyiacit/projeto/ProjetoApplication.java
+++ projeto/src/main/java/com/fluffyiacit/projeto/ProjetoApplication.java
@@ -3,6 +3,7 @@ package com.fluffyiacit.projeto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Anotation que realiza o Boot 
 @SpringBootApplication
 public class ProjetoApplication {
 
